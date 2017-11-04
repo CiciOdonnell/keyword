09274ef4b8fc148c57146f32bab67d5bad6457ce
ciciodonnell <ccod_2000@yahoo.com>
2017-11-04 08:28:30 -0700

    loads 2 example csv files

--- keyword.py
+++ keyword.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 print 'hello world'
 
+import csv
+with open("keyword.csv") as csvfile:
+   reader = csv.DictReader(csvfile)
+   for row in reader:
+     print(row['cluster'], row['keyword'])
+
